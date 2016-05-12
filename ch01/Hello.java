593a331e1724f76e1f00de5bbed7ed2d17b637e9
Thissy4May <dion_watts22@yahoo.co.uk>
2016-05-12 20:40:29 +0200

    Update Hello.java

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
-        System.out.print(" How are you?") // an extra string I added
+        System.out.print(" How are you?") // an extra line I added
     }
 
 }
